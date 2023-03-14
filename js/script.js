cd186824018b1bde0949f2eb495722ef2c0a51ea
Antonio <67365572+EnFuegoV3@users.noreply.github.com>
2023-03-13 20:28:55 -1000

    working on string equation using slice

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ const opp = document.querySelectorAll('.buttons > .op');
 const headEq = document.querySelector('.equation');
 
 let equation = [];
+let total = [];
 
 const clear = document.querySelector('#clear');
     clear.addEventListener('click', () => {
@@ -32,7 +33,8 @@ function operate() {
     let a = Number(equation[0]);
     let b = Number(equation[2]);
     let c = equation[1];
-    let outcome
+
+        let outcome
     if(c == '+'){
         outcome = addition(a, b);
     } else if(c == '-') {
@@ -42,7 +44,9 @@ function operate() {
     } else if(c == '/'){
         outcome = division(a,b).toFixed(5);
     }
+        // total.push([outcome]);
         return outcome
+        
 }
 
 let num1 = document.createElement('span');
@@ -52,15 +56,28 @@ let num2 = document.createElement('span');
 function operand() {
     opp.forEach(function(opp) {
         opp.addEventListener('click', (e) => {
+            if(equation.length >= 2) {
+                let btn = e.target.id;
+                equation.splice(2,2,[output.textContent]);
+                equation.splice(1, 1, [btn]);
+                headEq.append(output.textContent);
+                output.textContent = "";
+                operate();
+                headEq.append(e.target.id)
+                output.append(operate());
+                equation.splice(0,0,[output.textContent]);
+                console.log(equation);
+                console.log(total);
+            } else{
             equation.push([output.textContent]);
             let btn = e.target.id;
             output.appendChild(sign);
             sign.textContent = btn;
-            // equation.opp = [btn];
-            equation.push([btn])
+            equation.push([btn]);
             headEq.append(output.textContent);
             output.textContent = "";
-            // console.log(equation.opp);
+            console.log(equation);
+            }
         }) 
     });
 }
@@ -68,9 +85,18 @@ function operand() {
 function value(){
     numbers.forEach(function(numbers) {
         numbers.addEventListener('click', (e) => {
-            let btn = e.target.id;
-            output.append(btn);          
-            // console.log(equation.numbers);
+            if(equation.length >= 4){
+                // headEq.append(output.textContent);
+                
+                output.textContent = "";
+                let btn = e.target.id;
+                output.append(btn);          
+                console.log(equation);
+            } else {
+                let btn = e.target.id;
+                output.append(btn);          
+                console.log(equation);
+            }
         })  
     });
 }
@@ -82,8 +108,12 @@ equal.addEventListener('click', ()=>{
     operate();
     headEq.append('=')
     output.append(operate());
-    console.log(equation);
+    
 })
+
+function strinEq() {
+
+} 
 
 value();
 operand();
