754d843a71fa87f3d2b7dee15414f67ec35f3474
Antonio <67365572+EnFuegoV3@users.noreply.github.com>
2023-03-14 21:14:39 -1000

    working except stringing not hitting equals but operator

--- js/script.js
+++ js/script.js
@@ -4,7 +4,11 @@ const equal = document.querySelector('.equal');
 const opp = document.querySelectorAll('.buttons > .op');
 const headEq = document.querySelector('.equation');
 
-let equation = [];
+let firstNum
+let secondNum
+let operand
+let outcome
+let holler
 
 const clear = document.querySelector('#clear');
     clear.addEventListener('click', () => {
@@ -29,10 +33,10 @@ function division(x,y) {
 
 
 function operate() {
-    let a = Number(equation[0]);
-    let b = Number(equation[2]);
-    let c = equation[1];
-    let outcome
+    let a = Number(firstNum);
+    let b = Number(secondNum);
+    let c = operand;
+    
     if(c == '+'){
         outcome = addition(a, b);
     } else if(c == '-') {
@@ -40,7 +44,7 @@ function operate() {
     } else if(c == 'x'){
         outcome = multiply(a, b);
     } else if(c == '/'){
-        outcome = division(a,b).toFixed(5);
+        outcome = division(a,b).toFixed(4);
     }
         return outcome
 }
@@ -49,44 +53,79 @@ let num1 = document.createElement('span');
 let sign = document.createElement('span');
 let num2 = document.createElement('span');
 
-function operand() {
-    opp.forEach(function(opp) {
-        opp.addEventListener('click', (e) => {
-            equation.push([output.textContent]);
-            let btn = e.target.id;
-            output.appendChild(sign);
-            sign.textContent = btn;
-            // equation.opp = [btn];
-            equation.push([btn])
-            headEq.append(output.textContent);
-            output.textContent = "";
-            // console.log(equation.opp);
-        }) 
-    });
-}
-
-function value(){
-    numbers.forEach(function(numbers) {
+function display() {
+    if(firstNum === 'string' && output.textContent === 'string'){
+        opp.forEach(function(opp) {
+            opp.addEventListener('click', (e) => {
+                specialEquals();
+            
+            }) 
+        });
+    }
+    else {numbers.forEach(function(numbers) {
         numbers.addEventListener('click', (e) => {
             let btn = e.target.id;
             output.append(btn);          
-            // console.log(equation.numbers);
         })  
     });
+    opp.forEach(function(opp) {
+        opp.addEventListener('click', (e) => {
+            firstNum = output.textContent;
+            let btn = e.target.id;
+            output.appendChild(sign);
+            sign.textContent = btn;
+            operand = btn;
+            headEq.append(output.textContent);
+            output.textContent = ""; 
+        }) 
+    });
+    equal.addEventListener('click', ()=>{
+        secondNum = output.textContent;
+        headEq.append(output.textContent);
+        output.textContent = "";
+        operate();
+        headEq.append('=')
+        output.append(operate());
+        console.log(firstNum, secondNum, operand);
+    });
+    }
+        
+    
 }
 
-equal.addEventListener('click', ()=>{
-    equation.push([output.textContent]);
-    headEq.append(output.textContent);
-    output.textContent = "";
-    operate();
-    headEq.append('=')
-    output.append(operate());
-    console.log(equation);
-})
+// function value(){
+//     numbers.forEach(function(numbers) {
+//         numbers.addEventListener('click', (e) => {
+//             let btn = e.target.id;
+//             output.append(btn);          
+//         })  
+//     });
+// }
 
-value();
-operand();
+// equal.addEventListener('click', ()=>{
+//     secondNum = output.textContent;
+//     headEq.append(output.textContent);
+//     output.textContent = "";
+//     operate();
+//     headEq.append('=')
+//     output.append(operate());
+//     console.log(firstNum, secondNum, operand);
+// })
+
+function specialEquals()  {
+    equal.addEventListener('click', ()=>{
+        secondNum = output.textContent;
+        // headEq.append(output.textContent);
+        // output.textContent = "";
+        operate();
+        // headEq.append('=')
+        output.append(operate());
+        console.log(firstNum, secondNum, operand);
+        
+    }) 
+}
+// value();
+display();
 
 
 
